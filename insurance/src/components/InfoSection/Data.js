76897a31103bf599632f23c633d664a67aacd922
Axel Fernando Avila Fernandez <ax.avila@hotmail.com>
2021-04-04 20:57:12 -0600

    update

---
+++ insurance/src/components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Vlife Insurance',
+    headLine: 'Diferent types of Insurance',
+    description: 'We have diferent types of Insurance that adapts to each of your requirements ',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require ('')
+    alt: 'car',
+    dark: true,
+    primary: true,
+    darkText: false
+
+}
