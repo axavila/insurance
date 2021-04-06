a644e8bfabe4953c0cedea38aeb446ec5d9b4371
Axel Fernando Avila Fernandez <ax.avila@hotmail.com>
2021-04-06 11:24:37 -0600

    object3

--- insurance/src/components/InfoSection/Data.js
+++ insurance/src/components/InfoSection/Data.js
@@ -1,4 +1,5 @@
 
+
 export const homeObjOne = {
     id: 'about',
     lightBg: false,
@@ -9,8 +10,44 @@ export const homeObjOne = {
     description: 'We have diferent types of Insurance that adapts to each of your requirements ',
     buttonLabel: 'Get started',
     imgStart: false,
-    img: require ('../../images/data1.svg'),
-    alt: 'car',
+    img: require ('../../images/data2.svg').default,
+    alt: 'data',
+    dark: true,
+    primary: true,
+    darkText: false
+
+};
+
+export const homeObjTwo = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Vlife Insurance',
+    headLine: 'Diferent types of Insurance',
+    description: 'We have diferent types of Insurance that adapts to each of your requirements ',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require ('../../images/data2.svg').default,
+    alt: 'data',
+    dark: true,
+    primary: true,
+    darkText: false
+
+};
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Vlife Insurance',
+    headLine: 'Diferent types of Insurance',
+    description: 'We have diferent types of Insurance that adapts to each of your requirements ',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require ('../../images/data2.svg').default,
+    alt: 'data',
     dark: true,
     primary: true,
     darkText: false
--- insurance/src/pages/index.js
+++ insurance/src/pages/index.js
@@ -3,7 +3,7 @@ import Sidebar from '../components/SideBar';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
-import { homeObjOne } from '../components/InfoSection/Data';
+import { homeObjOne, homeObjThree, homeObjTwo } from '../components/InfoSection/Data';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -21,6 +21,8 @@ const Home = () => {
             <Navbar toggle={toggle} />
             <HeroSection />
             <InfoSection {...homeObjOne}/>
+            <InfoSection {...homeObjTwo}/>
+            <InfoSection {...homeObjThree}/>
             
         </>
     );
